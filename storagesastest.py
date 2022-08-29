09f9e53bcdc7eda5422028b492f1f7f4ebfba38b
grecoe <grecoe@microsoft.com>
2022-08-29 12:25:31 -0400

    SAS testing

--- storagesastest.py
+++ storagesastest.py
@@ -1,4 +1,11 @@
+#######################################################################
+# Use the BlobUri class to validate information about an Azure Storage
+# SAS tokenized URI to a container or blob. 
+#
+
+
 from utils.identifiers import BlobUri
+from urllib import parse
 
 user_delegated_sas = "https://makebelieve.blob.core.windows.net/container_name/blob_name.txt?sv=2021-06-08&&se=2022-08-24T21:07:20Z&ske=2023-08-24T17:07:20Z&skt=2022-08-19T13:07:20Z&spr=https&sig=NO_SIGNATURE_HERE"
 portal_sas = "https://makebelieve.blob.core.windows.net/container_name/blob_name.txt?sv=2021-06-08&&se=2023-08-24T21:07:20Z&st=2022-08-19T13:07:20Z&spr=https&sig=NO_SIGNATURE_HERE"
@@ -7,7 +14,7 @@ portal_container_sas = "https://makebelieve.blob.core.windows.net/container_name
 sas_collection = {
     "delegated" : BlobUri(user_delegated_sas),
     "portal" : BlobUri(portal_sas),
-    "container" : BlobUri(portal_container_sas) 
+    "container" : BlobUri(portal_container_sas)
 }
 
 for sas in sas_collection:
